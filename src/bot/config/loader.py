455b9f47350af455dc716e2475e4ef1fe12d0c56
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-10-24 21:29:43 +0300

    architecture updates + startup settings

--- src/bot/config/loader.py
+++ src/bot/config/loader.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.files import JSONStorage
 from pathlib import Path
@@ -7,4 +9,9 @@ from AuthoConclusionBot.settings import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
 storage = JSONStorage(f'{Path.cwd()}/{"fsm_data.json"}')
+loop = asyncio.get_event_loop()
 dp = Dispatcher(bot, storage=storage)
+
+
+if __name__ == "__main__":
+    storage.write(Path.cwd())
